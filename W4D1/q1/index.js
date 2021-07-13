2976cdc9fdc4878ec9816af40399e20bc68d30f2
Khulan NARANTSETSEG <khulan.nar@gmail.com>
2021-07-13 07:15:30 -0700

    5

--- W4D1/q1/index.js
+++ W4D1/q1/index.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 
-app.set('view engine', 'ejs');
-
-app.set('views', path.join(__dirname, "view"));
-
-app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        cookies: req.cookies
-    })
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
+
+app.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-app.post('/add', (req, res) => {
-    res.cookie(req.body.key, req.body.value);
-    res.redirect('/');
+app.post("/addCookie", (req, res, next) => {
+  res.cookie(req.body.key, req.body.value);
+  res.redirect("/");
+});
+
+app.post("/remove", (req, res, next) => {
+  if (req.cookies.testKey) res.clearCookie("testKey");
+  res.redirect("/");
 });
 
 app.listen(3000);
